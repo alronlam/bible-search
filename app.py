e71d2e5092401874383fcc5bbd23753bfd7b029f
alron <alron.jan.lam@gmail.com>
2022-12-27 15:26:04 +0800

    Adapt code to read from diff data source with diff versions

--- app.py
+++ app.py
@@ -60,16 +60,23 @@ def embed_or_load_dataset(texts, version):
 
 
 @st.cache
-def read_data(bible_csv, book_csv, agg_chapter=False):
-    book_df = pd.read_csv(book_csv)
-    verses_df = pd.read_csv(bible_csv)
-    df = pd.merge(verses_df, book_df, on="b")
+def read_data(bible_csv, metadata_csv, agg_chapter=False):
 
-    df = df[["id", "n", "c", "v", "t_x"]]
+    # There is one constant metadata file (metadata_csv),
+    #   and another csv file containing the actual verses in the specified version (bible_csv).
+    metadata_df = pd.read_csv(metadata_csv)
+    verses_df = pd.read_csv(bible_csv, escapechar="\\")
+    df = pd.merge(verses_df, metadata_df, on="b")
+    df = df.fillna("")  # Some verses are blank in some versions
 
+    df = df[["n", "c", "v", "t_x"]]
+
+    # The data sources used have this convention in the columns. 
+    # Renaming them here for ease of remembrance.
     col_rename = {"n": "book", "c": "chapter", "v": "verse", "t_x": "text"}
     df = df.rename(columns=col_rename)
 
+    # Create a human-friendly string of specifying a verse (e.g. Genesis 1:1)
     df["source"] = df.apply(
         lambda row: f"{row['book']} {row['chapter']}:{row['verse']}", axis=1
     )
@@ -113,8 +120,8 @@ def config():
     # Static configs
     ROOT_DIR = Path(os.path.abspath(os.path.dirname(__file__)))
     version_map = {
-        "KJV": "t_kjv.csv",
-        "ASV": "t_asv.csv",
+        "NIV": "niv.csv",
+        "ESV": "esv.csv",
     }
 
     # Configurable elements
